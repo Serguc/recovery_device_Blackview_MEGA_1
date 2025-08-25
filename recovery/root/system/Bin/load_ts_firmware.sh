b8cfebefa418ea0fa8ff452cb4a1fbd05f892041
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-25 21:32:04 +0300

    Update load_ts_firmware.sh

--- recovery/root/system/Bin/load_ts_firmware.sh
+++ recovery/root/system/Bin/load_ts_firmware.sh
@@ -1,2 +1,45 @@
 #!/sbin/sh
    insmod /vendor/firmware/Himax_firmware.bin
+   Input Device 4: mtk-tpd
+
+Descriptor:84931e976ab60191371c1c95baf408538ca4c4c5
+
+Generation: 23
+
+Location: built-in
+
+Keyboard Type: non-alphabetic
+
+Has Vibrator:false
+
+Has Sensor: false
+
+Has battery: false
+
+Has mic: false
+
+Sources: 0x1103 (keyboard touchscreen)
+
+AXIS_X: source=0x1002 min=0.0 max=1199.0 flat=0.0
+
+fuzz=0.0 resolution=0.0
+
+AXIS_Y: source=0x1002 min=0.0 max=1999.0 flat=0.0 fuzz=0.0 resolution=0.0
+
+AXIS_PRESSURE: source=0x1002 min=0.0 max=1.0 flat=0.0 fuzz=0.0 resolution=0.0
+
+AXIS_SIZE: source=0x1002 min=0.0 max=1.0 flat=0.0 fuzz=0.0 resolution=0.0
+
+AXIS_TOUCH_MAJOR: source=0x1002 min=0.0
+
+max=2332.3809 flat=0.0 fuzz=0.0 resolution=0.0
+
+AXIS_TOUCH_MINOR: source=0x1002 min=0.0
+
+max=2332.3809 flat 0.0 fuzz=0.0 resolution=0.0
+
+AXIS_TOOL_MAJOR: source=0x1002 min=0.0 max=2332.3809 flat=0.0 fuzz=0.0 resolution=0.0
+
+AXIS_TOOL MINOR: source=0x1002 min=0.0
+
+max=2332.3809 flat=0.0 fuzz=0.0 resolution=0.0
